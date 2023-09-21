69b0c79c35d2ef583b678a77ec87426c245b6918
Farjana Akhter <farjana.akhter085@gmail.com>
2023-09-21 15:27:59 -0500

    created package.json file and set up the details

---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+// const {Model, DataTypes} = require('sequelize');
+// const sequelize = require('../config/connection');
+
+// class Comment extends Model {}
+
+// Comment.init(
+//     {
+//         // define columns
+//         id: {
+//             type: DataTypes.INTEGER,
+//             primaryKey: true,
+//             allowNull: false,
+//             autoIncrement: true
+//         },
+//         user_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             references: {
+//                 model: 'user',
+//                 key: 'id'
+//             }
+//         },
+//         post_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             references: {
+//                 model: 'post',
+//                 key: 'id'
+//             }
+//         },
+//         comment_text: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             validate: {
+//             len: [3]  //  comment_text must be at least three characters 
+//             }
+//         },
+//     },
+//     {
+//         sequelize,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'comment'
+//     }
+// );
+
+
+// module.exports = Comment;
---
+++ seeds/comment-seeds.js
@@ -0,0 +1,33 @@
+// const { Comment } = require('../models');
+
+// const commentData = [
+//   {
+//     comment_text: "Awesome",
+//     user_id: 1,
+//     post_id: 1
+//   },
+//   {
+//     user_id: 5,
+//     post_id: 6,
+//     comment_text: "Very useful tool!"
+// },
+//   {
+//     comment_text: "Best of luck",
+//     user_id: 2,
+//     post_id: 2
+//   },
+//   {
+//     comment_text: "Fantastic",
+//     user_id: 3,
+//     post_id: 3
+//   },
+//   {
+//     user_id: 4,
+//     post_id: 4,
+//     comment_text: "This is great news!"
+// },
+// ];
+
+// const seedComments = () => Comment.bulkCreate(commentData);
+
+// module.exports = seedComments;
